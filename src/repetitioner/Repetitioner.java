6213efd70f3a01555ce22c539f1b507fc8c088f0
Analysatorn <analysatorn@tuc.se>
2021-02-14 23:31:58 +0100

    Initial commit.

---
+++ src/repetitioner/Repetitioner.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repetitioner;
+
+/**
+ *
+ * @author Kalle
+ */
+public class Repetitioner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        metodTestning();
+        
+        NewClass importeradKLass = new NewClass();
+        
+        System.out.println(importeradKLass.x);
+        
+        testClass test = new testClass();
+        System.out.println(test.color);
+        System.out.println(test.color);
+        System.out.println(test.color);
+        System.out.println("");
+        
+        System.out.println(test.color);
+ 
+       
+        
+        
+    }
+public static void metodTestning(){
+            for(int i = 0; i<10; i++){
+                System.out.println(i);
+            }
+           
+       }
+
+
+}
+
